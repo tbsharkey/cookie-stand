37fe8b76bdfdb7320e62fc2fcf5b8cefea6365d4
Tom Sharkey <tbsharkey@gmail.com>
2023-03-14 01:26:41 -0700

    Completed JS function

--- js/app.js
+++ js/app.js
@@ -4,4 +4,19 @@ let seattleStore = {
   minCustomers: 23,
   maxCustomers: 65,
   averageTicket: 6.3,
+
+  randomCustomerCount(min, max) {
+    let num = Math.random();
+    let range = max - min + 1;
+    let randomNumber = Math.floor(num * range) + min;
+    return randomNumber;
+  },
+
+  totalCookies() {
+    let customerCount = this.randomCustomerCount(this.minCustomers, this.maxCustomers);
+    let randomTicket = this.averageTicket * customerCount;
+    return Math.floor(randomTicket);
+  },
 };
+
+console.log(seattleStore.totalCookies());
